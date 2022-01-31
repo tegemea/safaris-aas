19f19caf03955dc106e5d6e34a8478d835dfbcc5
lema <lemajunior@hotmail.com>
2022-01-31 20:22:11 +0300

    refined destinations index view

--- pages/destinations/index.vue
+++ pages/destinations/index.vue
@@ -6,7 +6,7 @@
           <div class="card-deck">
             <div class="card mb-4">
               <div class="card-body p-0">
-                <NuxtLink :to="`/destinations/${destination.slug}`">
+                <NuxtLink :to="`/destinations/${destination.slug}`" :title="destination.name">
                   <img 
                     :src="`${baseURL}/storage/destination_photos/${destination.photo}`"
                     class="img-fluid"
@@ -21,7 +21,15 @@
               >
               </div>
               <div class="card-footer">
-                <NuxtLink :to="`/destinations/${destination.slug}`">{{ destination.name }}</NuxtLink>
+                <NuxtLink :to="`/destinations/${destination.slug}`" 
+                  class="text-decoration-none"
+                  :title="destination.name"
+                >
+                  <h4 class="thin-fonts text-black-50 m-0 ellipsis">
+                    <fai :icon="['fas','map-marker-alt']" class="mr-2 orange-color"></fai>
+                    {{ destination.name }}
+                  </h4>
+                </NuxtLink>
               </div>
             </div>
           </div>
